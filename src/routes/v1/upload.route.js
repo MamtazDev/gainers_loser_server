3a701022aba4a6d6380ad9316612bb8b7cf69197
MamtazDev <akter.mstmamtaz@gmail.com>
2024-05-08 10:21:05 +0600

    new  env

--- src/routes/v1/upload.route.js
+++ src/routes/v1/upload.route.js
@@ -7,6 +7,7 @@ const router = express.Router();
 router.post('/countryList', uploadController.countryList);
 router.get('/gain-looses', uploadController.GainLoses);
 
+
 router.post('/countryListExtractor', upload.single('country'), uploadController.countryList);
 
 module.exports = router;
